f8e623504bc3556b277a3dc5d2bd4fed18090e33
Ricardo Almeida <ricardo@grover.com>
2022-03-20 14:14:07 +0100

    Bump gateway

--- gateway/index.js
+++ gateway/index.js
@@ -1,8 +1,12 @@
 const { ApolloServer } = require("apollo-server");
-const { ApolloGateway } = require("@apollo/gateway");
+const { ApolloGateway, IntrospectAndCompose } = require("@apollo/gateway");
 
 const gateway = new ApolloGateway({
-  serviceList: [{ name: "federation-accounts", url: "http://localhost:4001" }],
+  supergraphSdl: new IntrospectAndCompose({
+    subgraphs: [
+      { name: "federation-accounts", url: "http://localhost:4001" },
+    ],
+  }),
 });
 
 const server = new ApolloServer({
